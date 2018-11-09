37961699837804acc6d528c52e8082b1014a0a6b
glebacque63 <guillaume.lebacque.tic@gmail.com>
2018-11-09 16:21:26 +0100

    Initial commit

---
+++ src/main/java/artefactid/idartefact/App.java
@@ -0,0 +1,24 @@
+package artefactid.idartefact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main(String[] args)
+	{
+		System.out.println(modulo(7, 5));
+		System.out.println(division(10, 4));
+	}
+	
+	public static int modulo(int a, int b)
+	{
+		return a%b
+	}
+	
+	public static double division(double a, double b)
+	{
+		return (double)a/b
+	}
+}
